cce9646d18424b6a4bbe2fa40af027106bacb856
Daniel <daniel38_14@hotmail.com>
2025-06-01 14:03:02 -0500

    Initial Commit

---
+++ InterTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InterTest
+//
+//  Created by Daniel Crespo Duarte on 1/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InterTest/InterTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterTestApp.swift
+//  InterTest
+//
+//  Created by Daniel Crespo Duarte on 1/06/25.
+//
+
+import SwiftUI
+
+@main
+struct InterTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
